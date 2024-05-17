d3bb09b8b96071eef24a1348a39e57a7c9b72e1e
Diyouf Kv <111433069+Diyouf@users.noreply.github.com>
2024-05-17 20:02:09 +0530

    Generate spelling corrected

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,7 +3,7 @@ const Button = (prop) => {
   return (
     <div>
       <button onClick={prop.handleAction}  className="bg-red-400 rounded-md p-2 mt-8 font-medium text-white">
-        Genarate
+        Generate
       </button>
     </div>
   );
